e0ae97b4a3b40550844f4c18051d3e4c9403edff
JesseChaucer <zhouhuiyumej@126.com>
2020-03-01 09:32:10 +0800

    优化命令行参数的使用

--- main.go
+++ main.go
@@ -3,23 +3,35 @@ package main
 import (
 	"fmt"
 	"flag"
+	"os"
 
 	"inAction/fileUtil/translate"
 )
 
 func main() {
 	// 从命令行获取参数
-	file := "";  // 文件路径
+	var filePath string  // 文件路径
+	/* filePath := "";  // 文件路径
 	operation := "";  // 操作
-	flag.StringVar(&file, "f", "default", "文件路径")
-	flag.StringVar(&operation, "o", "d", "删除操作")
+	flag.StringVar(&filePath, "f", "default", "文件路径")
+	flag.StringVar(&operation, "o", "d", "删除操作") */
+
+	flag.StringVar(&filePath, "d", "", "传入文件路径, 删除 @deprecated@ 字段")
+	flag.StringVar(&filePath, "r", "", "传入文件路径, 用英语替换其他语言")
 	flag.Parse()
 
+	// 获取命令行参数
+	fmt.Println("命令行参数数量:",len(os.Args))
+	for k,v:= range os.Args{
+			fmt.Printf("args[%v]=[%v]\n",k,v)
+	}
+	
+	operation := os.Args[1]
 	switch operation {
-	case "d":
-		translate.DeleteDeprecated(file)
-	case "r":
-		translate.ReplaceWithEn(file)
+	case "-d":
+		translate.DeleteDeprecated(filePath)
+	case "-r":
+		translate.ReplaceWithEn(filePath)
 	default: 
 		fmt.Println("没有对应操作")
 	}
--- translate/deleteDeprecated.go
+++ translate/deleteDeprecated.go
@@ -42,36 +42,4 @@ func DeleteDeprecated(filePath string) {
 	} else {
 		fmt.Println("delete success...")
 	}
-
-
-
-	/* input, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	lines := strings.Split(string(input), "\n")
-
-	var resSlice []string
-	for index, line := range lines {
-		if !strings.Contains(line, "@DEPRECATED@") {
-			// 如果下一行是要删除的行，并且是当前语言的最后一行(即最后没有逗号)，则当前行最后的逗号要去掉
-			if index+1 < len(lines) {
-				nextLine := lines[index+1]
-				if strings.Contains(nextLine, "@DEPRECATED@") && nextLine[len(nextLine)-1:] != "," {
-					line = strings.TrimRight(line, ",")
-				}
-			}
-			resSlice = append(resSlice, line)
-		}
-	}
-
-	// 覆盖原文件
-	output := strings.Join(resSlice, "\n")
-	err = ioutil.WriteFile(filePath, []byte(output), 0644)
-	if err != nil {
-		log.Fatalln(err)
-	} else {
-		fmt.Println("delete success...")
-	} */
 }
