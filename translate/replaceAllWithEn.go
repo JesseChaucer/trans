d35cb9d1a9305115b854a1ee13d88b310ce48af3
Jesse <vino@C02FM2DCQ05N.local>
2021-05-28 10:31:14 +0800

    perf: 优化替换函数

--- translate/replaceAllWithEn.go
+++ translate/replaceAllWithEn.go
@@ -5,10 +5,6 @@ package translate
 
 import (
 	"fmt"
-	"encoding/json"
-	"log"
-	"io/ioutil"
-	"bytes"
 
 	"inAction/trans/util"
 )
@@ -23,19 +19,6 @@ func ReplaceAllWithEn(filePath string) {
 		}
 	}
 
-	resByte, err := json.Marshal(langMap)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// 格式化JSON
-	var formattedBytesBuffer bytes.Buffer
-	json.Indent(&formattedBytesBuffer, resByte, "", "    ")
-
-	err = ioutil.WriteFile(filePath, []byte(formattedBytesBuffer.String()), 0644)
-	if err != nil {
-		log.Fatalln(err)
-	} else {
-		fmt.Println("replace all success...")
-	}
+	/* 把翻译的数据写回到多语言文件中 */
+	util.WriteFile(filePath, langMap)
 }
--- translate/replaceWithEn.go
+++ translate/replaceWithEn.go
@@ -5,10 +5,6 @@ package translate
 
 import (
 	"fmt"
-	"encoding/json"
-	"log"
-	"io/ioutil"
-	"bytes"
 
 	"inAction/trans/util"
 )
@@ -32,19 +28,6 @@ func ReplaceWithEn(filePath string) {
 		}
 	}
 
-	resByte, err := json.Marshal(langMap)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// 格式化JSON
-	var formattedBytesBuffer bytes.Buffer
-	json.Indent(&formattedBytesBuffer, resByte, "", "    ")
-
-	err = ioutil.WriteFile(filePath, []byte(formattedBytesBuffer.String()), 0644)
-	if err != nil {
-		log.Fatalln(err)
-	} else {
-		fmt.Println("replace 未翻译字段 success...")
-	}
+	/* 把翻译的数据写回到多语言文件中 */
+	util.WriteFile(filePath, langMap)
 }
