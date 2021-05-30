5638e3243daf95968a12efa0673f82be776e5c4d
Jesse <vino@C02FM2DCQ05N.local>
2021-05-30 11:29:19 +0800

    feat: 目录处理

--- process/fromTransSystem.go
+++ process/fromTransSystem.go
@@ -2,18 +2,18 @@
 package process
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"crypto/md5"
-	"encoding/hex"
 
 	"inAction/trans/def"
 	"inAction/trans/util"
 )
 
 // 获取api数据，返回对应的struct类型的地址
-func GetTransData(tranId string) *util.ResDataStruct {
+func getTransData(tranId string) *util.ResDataStruct {
 	var url = def.Api + tranId
 	fmt.Printf("Get data from: %s\n", url)
 	resp, err := http.Get(url)
@@ -36,11 +36,11 @@ func GetTransData(tranId string) *util.ResDataStruct {
 	}
 }
 
-/** 
+/**
  * 获取字符串的md5加密后的字符串
  * 如："7×24小时 专家支持" --> "7×24小时专家_0ecc"
  * 含中文字符，要先转成[]rune
-*/
+ */
 func GetMD5Text(text string) string {
 	var runeSlice = []rune(text)
 	if len(runeSlice) > 8 {
@@ -50,7 +50,7 @@ func GetMD5Text(text string) string {
 
 		// 去除字符串中的空格
 		for key, val := range runeSlice {
-			if (string(val) == " ") {
+			if string(val) == " " {
 				runeSlice = append(runeSlice[:key], runeSlice[key+1:]...)
 			}
 		}
@@ -58,16 +58,15 @@ func GetMD5Text(text string) string {
 		text = string(runeSlice[:8]) + "_" + md5Str[:4]
 	}
 	return text
-} 
-
+}
 
 // 翻译指定语言
 func trans(lang string, langMap util.LangType, tranSlice util.TransType) {
 	currentLangMap := langMap[lang]
 	for _, val := range tranSlice {
 		// fmt.Printf("%#v\n", val)
-		var cn = val.Text  // 中文
-		var translatedText = "";  // 对应语言的翻译
+		var cn = val.Text       // 中文
+		var translatedText = "" // 对应语言的翻译
 		switch lang {
 		case "en_US":
 			translatedText = val.EnUS
@@ -81,35 +80,60 @@ func trans(lang string, langMap util.LangType, tranSlice util.TransType) {
 			translatedText = val.RuKZ
 		}
 		var md5Text = GetMD5Text(cn)
-		if (len(translatedText) > 0) {
+		if len(translatedText) > 0 {
 			if _, ok := currentLangMap[md5Text]; ok {
 				// fmt.Printf("%v\n", md5Text)
-				currentLangMap[md5Text] = translatedText;
+				currentLangMap[md5Text] = translatedText
 			}
 		}
 	}
 }
 
-func FromTransSystem(filePath string, tranId string) {
+func useTransFunc(filePath string, tranId string) {
 	fmt.Println("---- 用指定文案翻译指定文件 ----")
-	
+
 	// 接口返回的翻译数据，转成结构体
-	tranStruct := GetTransData(tranId)
+	tranStruct := getTransData(tranId)
 	var tranSlice = tranStruct.Data.Trans
 
 	// 多语言json文件转成map
 	// filePath = "./asset/demo.json"
 	langMap := util.JsonToMap(filePath)
-	
+
 	for key, _ := range langMap {
 		// fmt.Printf("%#v\n", key)
 		// 不是简体和繁体，则翻译
-		if (key != "zh_Hans_CN" && key != "zh_Hant_HK") {
-			trans(key, langMap, tranSlice);
+		if key != "zh_Hans_CN" && key != "zh_Hant_HK" {
+			trans(key, langMap, tranSlice)
 		}
 	}
-	
 
 	/* 把翻译的数据写回到多语言文件中 */
 	util.WriteFile(filePath, langMap)
+}
+
+func FromTransSystem(filePath string, tranId string) {
+	/* 定义处理文件的函数 */
+	var processFunc = func(filePath string) {
+		fmt.Println("---- 用指定文案翻译指定文件 ----")
+
+		// 接口返回的翻译数据，转成结构体
+		tranStruct := getTransData(tranId)
+		var tranSlice = tranStruct.Data.Trans
+
+		// 多语言json文件转成map
+		langMap := util.JsonToMap(filePath)
+
+		for key, _ := range langMap {
+			// fmt.Printf("%#v\n", key)
+			// 不是简体和繁体，则翻译
+			if key != "zh_Hans_CN" && key != "zh_Hant_HK" {
+				trans(key, langMap, tranSlice)
+			}
+		}
+
+		/* 把翻译的数据写回到多语言文件中 */
+		util.WriteFile(filePath, langMap)
+	}
+	util.ProcessAllFile(filePath, processFunc)
 }
--- util/processAllFile.go
+++ util/processAllFile.go
@@ -7,16 +7,16 @@ import (
 )
 
 // 遍历文件夹下的所有文件，并处理
-func ProcessAllFile(pathname string, processFunc func(string)) error {
-	fmt.Printf("---- 当前目录：%s ----\n", pathname)
-	rd, err := ioutil.ReadDir(pathname)
+func ProcessAllFile(filePath string, processFunc func(string)) error {
+	rd, err := ioutil.ReadDir(filePath)
 	for _, fi := range rd {
 		/**
 		 * 1. 当前文件为目录
 		 * 2. 不以"."号开头（即不是隐藏文件）
 		 */
-		var currentFile = pathname + "/" + fi.Name()
+		var currentFile = filePath + "/" + fi.Name()
 		if fi.IsDir() && !strings.HasPrefix(fi.Name(), ".") {
+			fmt.Printf("---- 当前目录：%s ----\n", filePath)
 			ProcessAllFile(currentFile, processFunc)
 		} else if strings.HasSuffix(fi.Name(), "message.json") {
 			// 如果文件名后缀为 message.json，则处理
