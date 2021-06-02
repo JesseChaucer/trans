16c6e278317ceda4bda8bcb09d4b6b514a49862f
Jesse <vino@C02FM2DCQ05N.local>
2021-06-02 14:25:05 +0800

    feat: 更新命令行提示

--- def/data.go
+++ def/data.go
@@ -4,17 +4,17 @@ package def
 // 自定义命令行的帮助信息
 const HelpInfo = `
 1. 删除 @deprecated@ 字段
-    trans -d test.message.json
+    trans -d test.messages.json
     trans -d dir/
 
 2. 用英语替换其他语言 -- 只替换未翻译的字段(中文简体、繁体除外)
-    trans -r test.message.json
+    trans -r test.messages.json
 
 3. 用英语替换其他语言 -- 不管是否翻译，直接替换
-    trans -ra test.message.json
+    trans -ra test.messages.json
 
 4. 用指定文案翻译指定文件
-    trans -f test.message.json -id 104407325.1157
+    trans -f test.messages.json -id 104407325.1157
 `
 
 const Api = "http://trans.viabtc.com/api/trans/card/"
