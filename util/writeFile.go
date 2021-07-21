31cbdd675e9c28eae2de2a47db9edf4f51fab811
jesse <admin@C02FM2DCQ05N.local>
2021-07-21 10:50:59 +0800

    feat: update

--- util/writeFile.go
+++ util/writeFile.go
@@ -12,6 +12,14 @@ import (
 	"inAction/trans/def"
 )
 
+// Encode()方法会在文件末尾写入空行，下面去掉空行
+func delEmptyLine(file *os.File) {
+	// 相对文件末尾，向前移动1个字节
+	off , _ := file.Seek(-2, 2)
+	// 在指定位置写入字节切片
+	file.WriteAt([]byte(""), off)
+}
+
 func WriteFile(filePath string, langMap def.LangType) {
 	// 打开目标json文件
 	currentFile, err1 := os.OpenFile(
@@ -25,10 +33,12 @@ func WriteFile(filePath string, langMap def.LangType) {
 	defer currentFile.Close()
 
 	enc := json.NewEncoder(currentFile)
-	// 用四个空格缩进
-	enc.SetIndent("", "    ")
+	enc.SetIndent("", "    ") // 用四个空格缩进
 	enc.SetEscapeHTML(false) // 序列化时不转义特殊字符（如&）
 	err2 := enc.Encode(langMap)
+
+	delEmptyLine(currentFile)
+
 	if err2 != nil {
 		log.Fatalln(err2)
 	} else {
